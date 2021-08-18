fc4751c1b5a7fc5842f9cbe35d077487351c3499
Ken Cavagnolo <44750136+watchful-cluckets@users.noreply.github.com>
2021-08-18 13:49:02 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A demo of using cookiecutter https://drivendata.github.io/cookiecutter-data-science/',
+    author='Ken Cavagnolo',
+    license='MIT',
+)
